868e06797e3535ab98a72be53b8c33a3b47357d5
Lorenz Bischof <lorenzbischof@gmail.com>
2020-10-11 21:44:57 +0200

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,6 +36,6 @@ inotifywait -e create --recursive --monitor --format '%T' --timefmt '%s' "${SOUR
             # Fallback to GPSDateTime if DateTimeOriginal is not set
             # '-keywords<${directory;s#consume_test/##;$_ = undef if /^regex/}'
             # conditions: https://exiftool.org/forum/index.php?topic=3411.0
-            exiftool '-Directory<GPSDateTime' '-Directory<DateTimeOriginal' "-artist<\${directory;s/${SOURCE}\/?//;$_=undef if /^$/}" -d ${DESTINATION}/%Y -r ${SOURCE} || true
+            exiftool '-Directory<GPSDateTime' '-Directory<DateTimeOriginal' "-artist<\${directory;s#${SOURCE%/}/?##;$_=undef if /^$/}" -d ${DESTINATION}/%Y -r ${SOURCE} || true
         fi
     done
