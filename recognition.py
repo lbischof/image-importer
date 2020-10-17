79ef72f141ad2d7be26b2994aa98e62eac12d539
Lorenz Bischof <lorenzbischof@gmail.com>
2020-10-17 16:21:32 +0200

    Add debug messages

--- recognition.py
+++ recognition.py
@@ -9,15 +9,19 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 def test_image(image_to_check, known_names, known_face_encodings, tolerance=0.6):
         unknown_image = face_recognition.load_image_file(image_to_check)
         face_locations = face_recognition.face_locations(unknown_image)
+        print("Done face_locations")
         face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+        print("Done face_encoding")
         img = Image.open(image_to_check)
         width, height = img.size
 
         for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            print("Done compare_faces")
             name = "Unknown"
 
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            print("Done face_distance")
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_names[best_match_index]
